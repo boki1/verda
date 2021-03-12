0df08ae63bbad94ac24f57e0d964589279824cc2
aon2003 <a.o.naidenov@gmail.com>
2021-03-12 18:23:07 +0200

    lil changes

--- static/chat.js
+++ static/chat.js
@@ -1,7 +1,18 @@
 const socket = io.connect('http://' + document.domain + ':' + location.port);
 
 $(document).ready(function(){
+    let flag = true;
     $('#btnVerda').click( function() {
+        if (flag) {
+            flag = false;
+            $('div.row').hide();
+            // $('.header').animate({padding:'0 auto', position:'absolute, fixed', height:'100%', width:'100%'}, 'slow');
+
+        } else {
+            flag = true;
+            $('div.row').show();
+            // $('.header').animate({padding:'150px'}, 'fast');
+        }
         $('#chat_and_message').slideToggle();
         $('#quote').slideToggle();
     });
@@ -18,6 +29,7 @@ socket.on('connect', function() {
         }
     });
 })
+
 socket.on('print_message', function(message) {
     $('div#chat').append('<div><b>'+'You:'+'</b>'+message+'</div>')
 })
